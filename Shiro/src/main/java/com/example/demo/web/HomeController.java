048b2bc70e382bdba4a82a94b02de50ead3369f6
archerLj <lj0011977@163.com>
2017-07-04 14:56:00 +0800

    登录异常处理修改，异常信息加红

--- Shiro/src/main/java/com/example/demo/web/HomeController.java
+++ Shiro/src/main/java/com/example/demo/web/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import com.example.demo.config.Exception.IncorrectCaptchaException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.stereotype.Controller;
@@ -27,16 +28,17 @@ public class HomeController {
 
         // 登录失败从request中获取shiro处理的异常信息。
         // shiroLoginFailure:就是shiro异常类的全类名.
-        String exception = (String) request.getAttribute("shiroLoginFailure");
+        Exception exception = (Exception) request.getAttribute("shiroLoginFailure");
+//        String exception = (String) request.getAttribute("shiroLoginFailure");
         String msg = "";
         if (exception != null) {
-            if (UnknownAccountException.class.getName().equals(exception)) {
+            if (UnknownAccountException.class.isInstance(exception)) {
                 System.out.println("账户不存在");
-                msg = "UnknownAccountException => 账户不存在";
-            } else if (IncorrectCredentialsException.class.getName().equals(exception)) {
+                msg = "账户不存在或密码不正确";
+            } else if (IncorrectCredentialsException.class.isInstance(exception)) {
                 System.out.println("密码不正确");
-                msg = "IncorrectCredentialsException => 密码不正确";
-            } else if ("kaptchaValidateFailed".equals(exception)) {
+                msg = "账户不存在或密码不正确";
+            } else if (IncorrectCaptchaException.class.isInstance(exception)) {
                 System.out.println("验证码不正确");
                 msg = "验证码不正确";
             } else {
